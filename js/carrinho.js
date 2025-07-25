3dbe136d2f45e15686ddf8d0b59515cec3998ab6
Matheus Hiroshi <matheus.hiroshi@live.com>
2025-07-24 22:39:39 -0300

    cria js e puxa informações do botão do carrinho

---
+++ js/carrinho.js
@@ -0,0 +1,27 @@
+/*
+Objetivo 1 - quando clicar no botão de adicionar ao carrinho:
+    - atualizar o contador
+    - adicionar o produto no localStorage
+    - atualizar a tabela HTML do carrinho
+
+Objetivo 2 - remover produtos do carrinho:
+    - ouvir o botão de deletar
+    - remover do localStorage
+    - atualizar o DOM e o total
+
+Objetivo 3 - atualizar valores do carrinho:
+    - ouvir mudanças de quantidade
+    - recalcular total individual
+    - recalcular total geral
+*/
+
+const botoesAdicionarAoCarrinho = document.querySelectorAll('.adicionar-ao-carrinho');
+botoesAdicionarAoCarrinho.forEach(botao => {
+    botao.addEventListener('click', (evento) => {
+        const elementoProduto = evento.target.closest('.produto');
+        const produtoId = elementoProduto.dataset.id;
+        const produtoNome = elementoProduto.querySelector('.nome').textContent;
+        const produtoImagem = elementoProduto.querySelector('img').getAttribute('src');
+        const produtoPreco = parseFloat(elementoProduto.querySelector('.preco').textContent.replace('R$', '').replace('.','').replace(',', '.'));
+    })
+})
