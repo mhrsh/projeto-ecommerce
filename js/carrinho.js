6e25db61a3b00cdb510c60079c7e423ec8b3547a
Matheus Hiroshi <matheus.hiroshi@live.com>
2025-07-28 09:36:41 -0300

    adiciona funcionalidade de salvar produto no carrinho

--- js/carrinho.js
+++ js/carrinho.js
@@ -22,6 +22,37 @@ botoesAdicionarAoCarrinho.forEach(botao => {
         const produtoId = elementoProduto.dataset.id;
         const produtoNome = elementoProduto.querySelector('.nome').textContent;
         const produtoImagem = elementoProduto.querySelector('img').getAttribute('src');
-        const produtoPreco = parseFloat(elementoProduto.querySelector('.preco').textContent.replace('R$', '').replace('.','').replace(',', '.'));
-    })
+        const produtoPreco = parseFloat(elementoProduto.querySelector('.preco').textContent.replace('R$', '').replace('.', '').replace(',', '.'));
+
+        //buscar a lista de produts no localStorage
+        const carrinho = obterProdutosDoCarrinho();
+
+        //testar se o produto já existe no carrinho
+        const existeProduto = carrinho.find(produto => produto.id === produtoId);
+        //se existe produto, incrementar a quantidade
+        if (existeProduto) {
+            existeProduto.quantidade += 1;
+        } else {
+            //se não existir, adicionar produto com quantidade 1
+            const produto = {
+                id: produtoId,
+                nome: produtoNome,
+                imagem: produtoImagem,
+                preco: produtoPreco,
+                quantidade: 1
+            };
+            carrinho.push(produto);
+        }
+
+        salvarProdutosNoCarrinho(carrinho);
+    });
 })
+
+function salvarProdutosNoCarrinho(carrinho) {
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+}
+
+function obterProdutosDoCarrinho() {
+    const produtos = localStorage.getItem("carrinho");
+    return produtos ? JSON.parse(produtos) : [];
+}
